a8899b0703e59c6bbec4785a752ecb241f512242
Zenitos28 <dolgij.1990@mail.ru>
2022-12-18 11:41:20 +0800

    version 3

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,35 @@ public class Main {
     public static String calc(String input){
         String exception = "throws Exception"; // выдача исключения.
         Main action = new Main();
-        String[] array1 = input.split(" "); // разложили операцию по составляющим
+        String[] arihtExpr = input.split(" "); // разложили операцию по составляющим
         int a;
         int b;
-        if (array1.length != 3){
+        if (arihtExpr.length != 3){
             return exception+ " // т.к. формат математической операции не удовлетворяет заданию - две переменные и один оператор (+, -, /, *)";
         }  // проверка на количество переменных
-            boolean rimskie = false;
-        boolean rimskie1 = false;
-        boolean rimskie2 = false;
-       rimskie1 = action.rimPro(array1[0]);
-       rimskie2 = action.rimPro(array1[2]);
+        boolean rimskie = false;
+        boolean rimskie1 ;
+        boolean rimskie2 ;
+        rimskie1 = action.rimPro(arihtExpr[0]);
+        rimskie2 = action.rimPro(arihtExpr[2]);
         // Проверяем какие переменные арабские или римские.
-        if (rimskie1 == true && rimskie2 == false || rimskie1 == false && rimskie2 == true){
+        if ((rimskie1 && rimskie2 == false) || (rimskie1 == false && rimskie2 == true)){
             return exception+ " //т.к. используются одновременно разные системы счисления";
         }
         // Если переменные римские, то для проведения оперции приводим их в арабские и в вид целочисленных
         if(rimskie1 == true && rimskie2 == true) {
             rimskie = true;
-            array1[0] =Integer.toString(action.convRimInArab(array1[0]));
-            array1[2] = Integer.toString(action.convRimInArab(array1[2]));
+            arihtExpr[0] =Integer.toString(action.convRimInArab(arihtExpr[0]));
+            arihtExpr[2] = Integer.toString(action.convRimInArab(arihtExpr[2]));
         }
         // приводим числа в целочисленные значения
         try {
-        a = Integer.parseInt(array1[0]);
+        a = Integer.parseInt(arihtExpr[0]);
         } catch (NumberFormatException e) {
         return exception+"//т.к. введены не целочисленные значения";
         }
         try {
-        b = Integer.parseInt(array1[2]);
+        b = Integer.parseInt(arihtExpr[2]);
         } catch (NumberFormatException e) {
         return exception + "//т.к. введены не целочисленные значения";
         }
@@ -41,36 +41,42 @@ public class Main {
             return exception+ "//т.к. операторы не подходят по условия от 1 до 10";
             }
         // проверяем оператор и производим вычисление.
-        switch(array1[1]) {
-            case "+" -> result= a+b;
-            case "-" -> result= a-b;
-            case "/" -> result= a/b;
-            case "*" -> result= a*b;
-            default ->
+        switch(arihtExpr[1]) {
+            case "+" :
+                result= a+b;
+            break;
+            case "-" :
+                result= a-b;
+            break;
+            case "/":
+                result= a/b;
+            break;
+            case "*":
+                result= a*b;
+                break;
+            default:
                     {return exception + " //т.к. формат математической операции не удовлетворяет заданию - дву переменные и один оператор (+, -, /, *)";
                     }
                   }
               // Если числа были римские, то ответ переводим в римский вид
-          if (rimskie == true) {
+        if (rimskie) {
               if(result < 0){ output = exception + "// т.к. в римской системе нет отрицательных чисел";}
               else {
             output = action.convRimVArab(result);}
-            } else
-         {output=Integer.toString(result);}
-
-         return output;
+        } else
+            {output=Integer.toString(result);}
+        return output;
     }
 // распознаём, являются ли переменная римской
-        Boolean rimPro(String b){
-         Boolean a = false;
-         String[] rimAlf = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-         for (int i=0; i<rimAlf.length; i++){
-             if (b.equals(rimAlf[i])){
-                a = true;
-             }
+    Boolean rimPro(String b){
+    String[] rimAlf = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    for (String string:rimAlf){
+         if (b.equals(string)){
+             return true;
          }
-         return a;
-     }
+    }
+    return false;
+    }
      // Конвертация из арабских букв в римские для ввода от 1 до 10
     int convRimInArab(String arab){
         String[] rimAlf = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
@@ -78,6 +84,7 @@ public class Main {
         for (int i=0; i<rimAlf.length; i++){
             if (arab.equals(rimAlf[i])){
              z = i+1;
+             break;
             }
         }
         return z;
